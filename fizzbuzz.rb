106b60c0a6c4922fa69c9755bb1495a6eb572c1a
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-11 18:49:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz(number)
-  
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "Fizzbuzz"
+  else
+    nil
 end
